f2742a3bc259ef505e023694880ed3ee25b0ce9c
sahilsharma3942 <sahilsharma3942@gmail.com>
2024-06-08 02:15:44 +0530

    Wishlist bugs for empty wishlist

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -4,14 +4,16 @@ import { Card } from './Card';
 
 export const Wishlist = () => {
 
-    const [wishlistProducts, setWishlistProducts]= useState([])
+    const [wishlistProducts, setWishlistProducts]= useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(()=>{
         axios.get("https://fashion-flow-backend.onrender.com/api/v1/user/me/wishlist",{
             headers:{
                 Authorization:localStorage.getItem("token")
             }
         }).then((res)=>{console.log(res.data.wishlist.products)
-            setWishlistProducts(res.data.wishlist.products)
+            setWishlistProducts(res.data.wishlist.products);
+            setLoading(false);
         })
         .catch((err)=>{console.log(err.response.data.message)})
     },[])
@@ -28,13 +30,29 @@ export const Wishlist = () => {
         )}
 
     return(
-        wishlistProducts.length==0?<div><h1>...loading</h1></div>:
-        <div className='flex flex-wrap p-4'>{wishlistProducts.map((product,index)=>{
-            return <Card item={product.product} 
-                         key={index} 
-                         wishlist={wishlistProducts} 
-                         addToWishlist={handleAdd} 
-                         removeFromWishlist={handleRemove}/>
-        })}</div>
+        
+            <div>
+                {
+                    loading==true?<div>...loading</div>:
+                    <div className='flex flex-wrap p-4'>{wishlistProducts.length>0?(
+                        <div>
+                            {
+                                wishlistProducts.map((product,index)=>{
+                                    return <Card item={product.product} 
+                                                key={index} 
+                                                wishlist={wishlistProducts} 
+                                                addToWishlist={handleAdd} 
+                                                removeFromWishlist={handleRemove}/>
+                                })
+                            }
+                        </div>
+                    ):(
+                        <div>
+                            <h3>Your Wishlist is empty</h3>
+                        </div>
+                    )}</div>
+                }
+            </div>
+        
     )
 }
